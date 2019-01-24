4cda9e5e9da3071b3cd8abc25af280e5cfc05024
Dhiraj <dhiraj.jamia@gmail.com>
2019-01-24 11:33:11 +0530

    Create Chapter01.py

---
+++ venv/Scripts/Chapter01.py
@@ -0,0 +1,8 @@
+import Bio
+from Bio.Seq import Seq
+print(Bio._Version_)
+my_sequence=Seq('AGTATACTATGTGCATAGTCAGTCAGTCGA')
+print(my_sequence)
+print(my_sequence.alphabet)
+my_sequence_complement=my_sequence.complement
+print(my_sequence_complement)
